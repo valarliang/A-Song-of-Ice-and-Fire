fa0f539222f3c4e23ab06dbfdefce0546f9e4071
梁琰沁 <1780304097@qq.com>
2018-04-06 21:59:56 +0800

    final

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,15 +1,15 @@
 import {applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 
-const logger=(store)=>(next)=>(action)=>{
-	console.group(action.type)
-	const result=next(action)
-	console.log('The new state:',store.getState())
-	console.groupEnd()
-	return result
-}
+// const logger=(store)=>(next)=>(action)=>{
+// 	console.group(action.type)
+// 	const result=next(action)
+// 	console.log('The new state:',store.getState())
+// 	console.groupEnd()
+// 	return result
+// }
 
 export default applyMiddleware(
 	thunk,
-	logger
+	// logger
 )
