68b4ea050b307f87c8339794eee1422593f00154
梁琰沁 <valaryanqin@gmail.com>
2019-04-06 11:18:36 +0800

    deploy the gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component,Fragment } from 'react'
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {HashRouter as Router, Route, Switch } from 'react-router-dom'
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions'
 import Home from './Home'
